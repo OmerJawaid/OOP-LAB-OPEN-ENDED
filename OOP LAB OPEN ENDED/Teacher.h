42564c32c056d20411b2cb1fad047991304e0daa
Ahmad Iftikhar <ahmadiftikhar200536@gmail.com>
2024-03-21 22:44:49 +0500

    Teacher class Final

--- OOP LAB OPEN ENDED/Teacher.h
+++ OOP LAB OPEN ENDED/Teacher.h
@@ -1,24 +1,3 @@
-// #pragma once
-
-// #include <string>
-// #include <vector>
-
-// class Course; 
-
-// class Teacher {
-// private:
-//     int teacherID;
-//     std::string name, email;
-//     std::vector<Course*> coursesAssigned; 
-
-// public:
-//     Teacher(std::string n, int ID, std::string em);
-//     std::string getTeacherName() const; 
-//     void assignCourses(Course* course); 
-//     void removeCourse(Course* course); 
-//     void viewCourses() const; 
-// };
-
 #pragma once
 
 #include <string>
